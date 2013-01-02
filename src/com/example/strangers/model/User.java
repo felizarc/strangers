61cd347b2b37149806010e5f94bd976a7b359fe2
Xavier <Xavier@workrabbit>
2013-01-02 15:12:34 +0100

    Add User class.

--- src/com/example/strangers/model/User.java
+++ src/com/example/strangers/model/User.java
@@ -1,5 +1,48 @@
 package com.example.strangers.model;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	
+	/* ******************** *
+	 * ****** Fields ****** *
+	 * ******************** */
+	
+	private String login;
+	private ArrayList<Account> accountList;
+	
+	
+	/* ************************* *
+	 * ****** Constructor ****** *
+	 * ************************* */
+	
+	public User(String login, ArrayList<Account> accountList) {
+		this.login = login;
+		this.accountList = accountList;
+	}
+	
+	/* ********************* *
+	 * ****** Getters ****** *
+	 * ********************* */
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public ArrayList<Account> getAccountList() {
+		return accountList;
+	}
+	
+	/* ********************* *
+	 * ****** Setters ****** *
+	 * ********************* */
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public void setAccountList(ArrayList<Account> accountList) {
+		this.accountList = accountList;
+	}
+	
 }
